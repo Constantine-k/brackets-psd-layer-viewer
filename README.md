76fb74d57442f8f25a59544b5e20cd9f90d9418a
Konstantin Konstantinov <konstantin.k.bm@gmail.com>
2017-07-07 02:00:03 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# PSD Layer Viewer
+PSD Layer Viewer is an extension for the code editor Brackets that allows to view PSD files, export layers, generate CSS code.
+
+It is a very simple extension that shows panel with site [http://psd.keyangxiang.com/](http://psd.keyangxiang.com/) which is an awesome tool to work with PSD files.
+
+The extension have one little issue: "Export PNG" button doesn't work, so you should use drag and drop for saving image above the button.
+
+### Installation
+
+PSD Layer Viewer is installed from the Brackets Extension Manager.
+
+### Acknowledgements
+
+Author of [Online PSD Viewer](http://psd.keyangxiang.com/): [Keyang Xiang](http://keyangxiang.com/)
---
+++ main.js
@@ -0,0 +1,35 @@
+define(function(require, exports, module) {
+	
+    var CommandManager = brackets.getModule("command/CommandManager"),
+    Menus = brackets.getModule("command/Menus"),
+    PanelManager = brackets.getModule("view/PanelManager"),
+    ExtensionUtils = brackets.getModule("utils/ExtensionUtils"),        
+    AppInit = brackets.getModule("utils/AppInit");
+
+    var LAYERS_EXECUTE = "layers.execute";
+    var panel;
+    var panelHtml = require("text!panel.html");
+	var $layersIcon = $('<a href="#" title="PSD Layer Viewer" id="brackets-psd-layers-icon"></a>');
+
+    function handleLayers() {
+        if(panel.isVisible()) {
+            panel.hide();
+			$layersIcon.removeClass('active');
+            CommandManager.get(LAYERS_EXECUTE).setChecked(false);
+        } else {
+            panel.show();
+			$layersIcon.addClass('active');
+            CommandManager.get(LAYERS_EXECUTE).setChecked(true);
+        }
+    }	
+
+    AppInit.appReady(function () {
+        $layersIcon.click(function () {
+            handleLayers();
+        }).appendTo("#main-toolbar .buttons");
+		
+		ExtensionUtils.loadStyleSheet(module, "styles.css");
+        panel = PanelManager.createBottomPanel(LAYERS_EXECUTE, $(panelHtml),200);
+    });
+	
+});
